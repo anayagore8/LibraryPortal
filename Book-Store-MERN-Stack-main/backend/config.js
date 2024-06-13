fe26bf3cbf2644fd1cbd0d24941e9ddc8d8b4ae0
anayagore8 <anayagore0502@gmail.com>
2024-06-13 11:00:07 +0530

    First Commit

---
+++ Book-Store-MERN-Stack-main/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://Anaya:anayagore8@anayagore.tgrsygc.mongodb.net/?retryWrites=true&w=majority&appName=AnayaGore';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
